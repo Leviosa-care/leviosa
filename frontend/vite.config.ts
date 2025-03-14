7fa611a6555228fee195ddae2af43ddff472fada
GaryHY <henry.gary@hotmail.com>
2025-03-14 19:56:07 +0100

    frontend: remove dev dependency vite-plugin-mkcert

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import type { Plugin } from 'vite';
 import { kitRoutes } from 'vite-plugin-kit-routes';
-import mkcert from 'vite-plugin-mkcert';
 
 const add_browser_onmount: Plugin = {
     name: 'vite-plugin-onmount',
@@ -20,12 +19,11 @@ const add_browser_onmount: Plugin = {
 };
 
 export default defineConfig({
-    plugins: [sveltekit(), add_browser_onmount, kitRoutes(), mkcert()],
+    plugins: [sveltekit(), add_browser_onmount, kitRoutes()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
         environment: 'jsdom',
         setupFiles: ['src/vitest.setup.ts'],
         globals: true
     },
-    server: { https: true }
 });
