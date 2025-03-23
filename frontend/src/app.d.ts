3376924b96f9d0a0755cc5426be8424c3c226939
GaryHY <henry.gary@hotmail.com>
2025-03-23 11:28:13 +0100

    frontend: add picture, address1 and address2 fields in app global types definition

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,27 +1,29 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			// TODO: remove the "?" when done with the code
-			user: {
-				email?: string;
-				role: string;
-				lastname?: string;
-				firstname?: string;
-				gender?: string;
-				birthdate?: string;
-				telephone?: string;
-				address?: string;
-				city?: string;
-				postalCode?: number;
-			};
-		}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        interface Locals {
+            // TODO: remove the "?" when done with the code
+            user: {
+                email?: string;
+                role: string;
+                lastname?: string;
+                firstname?: string;
+                gender?: string;
+                birthdate?: string;
+                telephone?: string;
+                picture?: string;
+                address1?: string;
+                address2?: string;
+                city?: string;
+                postalCode?: number;
+            };
+        }
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
 }
 
-export {};
+export { };
