7658d982e65273ca9da7975b91cd5ec93fac43aa
GaryHY <henry.gary@hotmail.com>
2025-05-12 12:14:47 +0200

    feat: New mailService method uses defined repo and add 'from' field

--- backend/internal/domain/mail/service.go
+++ backend/internal/domain/mail/service.go
@@ -11,23 +11,23 @@ import (
 )
 
 type Service interface {
-	HandlePasswordForgotten(to string) error
+	HandlePasswordForgotten(ctx context.Context, to string) error
 	NewEvent(ctx context.Context, users []*models.User, eventTime string) error
 	NewPayment(ctx context.Context, user *models.User, eventTime string) error
 	NewVote(ctx context.Context, user *models.User, eventTime string) error
-	PendingUser(ctx context.Context, user *models.User) error
 	SendRegistrationReminderEmail(ctx context.Context, user *models.User, registrationName string, daysLeft int) error
-	SendOTP(ctx context.Context, email, firstname string, otp *otpService.OTP) error
-	WelcomeUser(ctx context.Context, user *models.User) error
+	SendOTP(ctx context.Context, email string, otp *otpService.OTP) error
+	WelcomeUser(ctx context.Context, email string, user *models.User, legalAddress, companyInstagram string) error
 }
 
 type service struct {
 	from     string
 	email    string
 	password string
+	repo     Reader
 }
 
-func New() (Service, error) {
+func New(ctx context.Context, repo Reader) (Service, error) {
 	email := os.Getenv("GMAIL_EMAIL")
 	if email == "" {
 		return nil, domain.NewNotFoundErr(fmt.Errorf("environment variable 'GMAIL_EMAIL'"))
@@ -36,8 +36,13 @@ func New() (Service, error) {
 	if password == "" {
 		return nil, domain.NewNotFoundErr(fmt.Errorf("environment variable 'GMAIL_PASSWORD'"))
 	}
+	from, err := repo.GetCompanyEmail(ctx)
+	if err != nil {
+		return nil, domain.NewNotFoundErr(fmt.Errorf("environment variable 'GMAIL_PASSWORD'"))
+	}
 	return &service{
-		from:     "contact@leviosa.care",
+		// from:     "contact@leviosa.care",
+		from:     from,
 		email:    email,
 		password: password,
 	}, nil
