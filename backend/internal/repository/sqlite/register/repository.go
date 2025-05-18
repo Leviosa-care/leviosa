7ce29e7e405bee0c082cd297e01b9ce83fece11b
GaryHY <henry.gary@hotmail.com>
2025-05-18 13:38:44 +0200

    fix(registerRepository): rename repository struct from 'Repository' to 'repository' for consistency

--- backend/internal/repository/sqlite/register/repository.go
+++ backend/internal/repository/sqlite/register/repository.go
@@ -3,14 +3,12 @@ package registerRepository
 import (
 	"context"
 	"database/sql"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type repository struct {
 	DB *sql.DB
 }
-
 func (r *repository) GetDB() *sql.DB {
 	return r.DB
 }
@@ -18,7 +16,6 @@ func (r *repository) GetDB() *sql.DB {
 func New(ctx context.Context, db *sql.DB) *repository {
 	return &repository{db}
 }
-
 // NOTE: old API
 
 //	func (s *Store) CreateRegistration(registration *types.Registration) error {
