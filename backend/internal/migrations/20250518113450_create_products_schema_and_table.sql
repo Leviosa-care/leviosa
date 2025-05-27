04b87756e5006e19735bc369b41ba97f12996266
GaryHY <henry.gary@hotmail.com>
2025-05-27 19:04:02 +0200

    wip: add some schema for production migrations

--- backend/internal/migrations/20250518113450_create_products_schema_and_table.sql
+++ backend/internal/migrations/20250518113450_create_products_schema_and_table.sql
@@ -1,9 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS products.products (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS offers (
+    id TEXT PRIMARY KEY,
+    product_id TEXT NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL UNIQUE,
+    picture_encrypted TEXT NOT NULL UNIQUE,
+    duration INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    price_id_encrypted TEXT NOT NULL UNIQUE
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE products.products;
+DROP TABLE products.offers;
 -- +goose StatementEnd
