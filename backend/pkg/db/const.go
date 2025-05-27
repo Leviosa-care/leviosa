f7d8d2e7bf65432cec8fce8dd944dfc91e886d51
GaryHY <henry.gary@hotmail.com>
2025-05-27 07:50:27 +0200

    feat: fix DefaultDB to make it more general for every database type use

--- backend/pkg/db/const.go
+++ backend/pkg/db/const.go
@@ -1,7 +1,7 @@
 package db
 
 // constants
-const DefaultDB = "leviosa.db"
+const DefaultDB = "leviosa"
 
 // To write custom update queries based existence or non existence of zero values
 type SQLMappable interface {
