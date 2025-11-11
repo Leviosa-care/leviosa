861cd05169034040e1a92781b544badc6e5c7850
GaryHY <henry.gary@hotmail.com>
2025-11-11 20:52:09 +0100

    refactor(booking): add crypto service to RoomService constructor
    - Add encx.CryptoService parameter to RoomService constructor
    - Store crypto service instance in RoomService struct
    - Update New function signature to accept crypto dependency

--- backend/internal/booking/application/room/service.go
+++ backend/internal/booking/application/room/service.go
@@ -9,18 +9,21 @@ import (
 	"github.com/Leviosa-care/leviosa/backend/internal/booking/ports"
 	"github.com/Leviosa-care/leviosa/backend/internal/common/errs"
 	"github.com/google/uuid"
+	"github.com/hengadev/encx"
 )
 
 type RoomService struct {
 	roomRepo     ports.RoomRepository
 	buildingRepo ports.BuildingRepository
+	crypto       encx.CryptoService
 }
 
 // New creates a new instance of the room service
-func New(roomRepo ports.RoomRepository, buildingRepo ports.BuildingRepository) ports.RoomService {
+func New(roomRepo ports.RoomRepository, buildingRepo ports.BuildingRepository, crypto encx.CryptoService) ports.RoomService {
 	return &RoomService{
 		roomRepo:     roomRepo,
 		buildingRepo: buildingRepo,
+		crypto:       crypto,
 	}
 }
 
