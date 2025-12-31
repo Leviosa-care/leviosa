c4973b5bd8c9b540ff667d221f705795822ba4df
GaryHY <henry.gary@hotmail.com>
2025-12-31 16:12:03 +0100

    chore(routes): preserve old app layout code as backups

---
+++ frontend/src/routes/(app)/old.layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { LayoutProps } from "./$types";
+    import { setUserContext } from "$lib/context/user.svelte";
+    import "../../app.css";
+
+    let { data, children }: LayoutProps = $props();
+
+    setUserContext(data.user);
+</script>
+
+<div class="flex flex-col">
+    {@render children()}
+</div>
