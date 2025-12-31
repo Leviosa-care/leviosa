2ad6c38fa3ac66f97c1d4b65c98bc384fd11794a
GaryHY <henry.gary@hotmail.com>
2025-12-31 16:31:55 +0100

    chore(marketing): preserve old component iterations

---
+++ frontend/src/routes/(marketing)/_old.testimonies.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Button from "$lib/ui/Button.svelte";
+</script>
+
+<div class="grid gap-4 p-24">
+    <p class="text-dark-900 text-5xl">
+        “The most comprehensive approach to health I’ve ever experienced. The
+        coaches genuinely care about your long-term success.”
+    </p>
+    <div>
+        <p>Sarah Jenkins</p>
+        <p>Physical Training Client</p>
+    </div>
+    <Button>Meet Our Team</Button>
+</div>
---
+++ frontend/src/routes/(marketing)/_old.why.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    interface Card {
+        title: string;
+        description: string;
+    }
+    const serviceCards: Card[] = [
+        {
+            title: "Massage Therapy",
+            description:
+                "Retroactive treatment designed for recorvery and stress relied tailored to your body's need.",
+        },
+        {
+            title: "Mindset Coaching",
+            description:
+                "Unlock your full potential with proven mental clarity strategies, guided mentality, and coginitive restructuring.",
+        },
+        {
+            title: "Physical Training",
+            description:
+                "Personalized strength and conditionning  programs built for peak performance, longevity, and functional movement.",
+        },
+    ];
+</script>
+
+<div class="bg-dark-50 p-24">
+    <div>
+        <h2 class="text-3xl font-bold">Why Choose Leviosa</h2>
+        <p class="text-dark-700">
+            We believe in sustainable, holistic results driven by expers
+            guidance and a supportive community
+        </p>
+    </div>
+
+    <div>
+        <ul class="flex items-center justify-between list-none">
+            {#each serviceCards as item}
+                {@render card(item.title, item.description)}
+            {/each}
+        </ul>
+    </div>
+</div>
+
+{#snippet card(title: string, description: string)}
+    <li class="grid gap-2 p-4 w-full">
+        <div class=" bg-dark-100 w-full aspect-square rounded-input"></div>
+        <p class="text-dark-900">{title}</p>
+        <p class="text-dark-500">{description}</p>
+    </li>
+{/snippet}
