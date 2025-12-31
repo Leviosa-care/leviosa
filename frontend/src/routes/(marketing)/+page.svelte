f3b82d7b9e184fe066d842bb5d76c28b1a8ce3e6
GaryHY <henry.gary@hotmail.com>
2025-12-31 16:23:17 +0100

    feat(marketing): compose landing page from sections

---
+++ frontend/src/routes/(marketing)/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Hero from "./_hero.svelte";
+    import Services from "./_services.svelte";
+    import Why from "./_why.svelte";
+    import Testimonies from "./_testimonies.svelte";
+    import Ready from "./_ready.svelte";
+
+    // TODO: After fetching from +page.server.ts page.
+    // import type { PageProps } from "./$types";
+    // let { data }: PageProps = $props();
+    // let { cards } = data
+</script>
+
+<main class="relative pt-24 lg:pt-32 overflow-hidden">
+    <Hero />
+    <Services />
+    <Why />
+    <Testimonies />
+    <Ready />
+</main>
